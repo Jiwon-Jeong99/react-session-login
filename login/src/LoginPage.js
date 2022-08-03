158b4462960a7c66345dd68abcd4259918a711b5
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-03 10:14:49 +0900

    done

--- login/src/LoginPage.js
+++ login/src/LoginPage.js
@@ -8,12 +8,11 @@ const LoginPage = () => {
   const [isLogin, setIsLogin] = useState(false);
   const [userEmail, setUserEmail] = useState("");
 
-  const onChangeusername = useCallback((e) => {
+  const onChangeUserName = useCallback((e) => {
     setUserName(e.target.value);
   }, []);
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value);
-    // 지금 입력한 값이랑 passwordcheck이랑 다르면? mismatcherror
   }, []);
 
   //로그인 버튼
@@ -29,7 +28,7 @@ const LoginPage = () => {
       };
 
       //  fetch와 같음 - post로 email, password 보냄
-      //  axios
+      //  fetch로 json형태로 http 헤더에 담아 user 정보를 json을 문자열 형태 변신해서 주세요.
       fetch("http://127.0.0.1:8000/auth/login", {
         method: "POST",
         mode: "cors",
@@ -38,15 +37,17 @@ const LoginPage = () => {
           // content-type : 해당 형태로
           "Content-Type": "application/json",
         },
+        // user 정보를 보내주는데, json을 문자열로 변환한 상태로
         body: JSON.stringify(user),
       })
         .then((res) => res.json())
         .then((data) => {
-          setUserEmail(data.data.email);
-          setUserName(data.data.userName);
+        //   setUserEmail(data.data.email);
+        //   setUserName(data.data.userName);
           setCookie("email", data.data.email);
           setCookie("userName", data.data.user_name);
         })
+        // 에러가 나면 403, 로그인 페이지 정보를 지워줌
         .catch((error) => {
           setUserName("");
           setPassword("");
@@ -77,10 +78,10 @@ const LoginPage = () => {
               <div>
                 <input
                   type="text"
-                  id="email"
-                  name="email"
+                  id="name"
+                  name="name"
                   value={userName}
-                  onChange={onChangeusername}
+                  onChange={onChangeUserName}
                 />
               </div>
             </label>
--- login/src/success.js
+++ login/src/success.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Success = ({userName}) => {
+const Success = ({ userName, email }) => {
   return (
-    <div>{userName}success 로그인에 성공하셨습니다!!</div>
-  )
-}
+    <>
+      <div>{userName}success 로그인에 성공하셨습니다!!</div>
+      <div>{email}</div>
+    </>
+  );
+};
 
-export default Success
+export default Success;
